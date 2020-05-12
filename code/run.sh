87f6fdc67215740731cff2a3c6e21a48a7bfdded
Alexander Acker <alexander.acker@tu-berlin.de>
2020-05-12 20:50:16 +0200

    Added bash run script.

---
+++ code/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+home=`dirname $(readlink -e $0)`
+
+start=$1
+end=$2
+
+device=$3
+
+for i in $(seq $start $end); do 
+    screen -S test -d -m sh -c "python3.6 $home/script.py --index $i --device $device; exec bash"
+done
+
