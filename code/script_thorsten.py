58f283a95f46f57aee75751c5fa160ee10508be2
twittkopp <62097606+twittkopp@users.noreply.github.com>
2020-05-19 17:13:12 +0200

    implemented some ideas from alex
    scaling from 0 to 100 and MSE was better in many cases than 0 to 1 and F1

--- code/script_thorsten.py
+++ code/script_thorsten.py
@@ -2,8 +2,8 @@ import pandas as pd
 import sqlite3 as lite
 import torch
 import torch.nn as nn
+import time
 from sklearn.metrics import r2_score
-import seaborn as sns; sns.set()
 
 
 def get_dataframe(db_connection, data_id, min_t, max_t):
@@ -33,7 +33,11 @@ def interpolate(test_df):
 
 def scaling(df):
     maximum = df['Mean'].max()
+    maximum_close = df['Close'].max()
+    if maximum_close > maximum:
+        maximum = maximum_close
     df['Mean'] = df['Mean'].apply(lambda x: (x/maximum)*100)
+    df['Close'] = df['Close'].apply(lambda x: (x/maximum)*100)
     return df, maximum
 
 
@@ -106,10 +110,13 @@ class Model(nn.Module):
 
 
 def train_models(model, data_id_test, train_data, epochs, device):
+    start = time.time()
+
     prediction_series = data_id_test
     print('######### Training {} #########'.format(prediction_series))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    #loss_function = torch.nn.L1Loss()
     loss_function = torch.nn.MSELoss()
     model.cuda(device)
     losses = []
@@ -117,17 +124,20 @@ def train_models(model, data_id_test, train_data, epochs, device):
     for e in range(epochs):
         total_loss = 0.0
         for x, y in train_data:
+            #print("training_x: " + str(x) + " training_y: " + str(y))
             x_tensor = torch.tensor(x, dtype=torch.float).cuda(device)
             optimizer.zero_grad()
             out = model(x_tensor.cuda(device))
             loss = loss_function(input=out, target=torch.tensor(y, dtype=torch.float).cuda(device))
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
+            total_loss += loss.item()  # r2_score(b_labels.cpu().detach().numpy(), out.cpu().detach().numpy())
 
         losses.append(round(total_loss, 6))
         print("Epoch {} loss: {:.6f}".format(e, total_loss / len(train_data)))
 
+    end = time.time()
+    print('========= Training took {:.2f} ========='.format(end - start))
     return losses
 
 
@@ -139,6 +149,7 @@ def predict_data(model, test_data, device, m):
             output = model(torch.tensor(x, dtype=torch.float).cuda(device))
             output = float(scaling_inv(output, m))
             predictions.append(output)
+            #print("predict_input: " + str(x) + " prediction: " + str(output))
     return predictions
 
 
@@ -154,13 +165,14 @@ def run(start=0, end=10000, epochs=6, device=0):
     ids = ids[start:end]
 
     r2_total = 0
+    r2_baseline_total = 0
 
     submission_results = pd.DataFrame()
     for data_id_test in ids:
         # Series and host
         result = data_id_test.split("#")
 
-        train_df = get_dataframe(db_connection, data_id_test, '2019-12-03 11:00:00', '2020-02-20 10:00:00')  # first two weeks will be cut in get_samples
+        train_df = get_dataframe(db_connection, data_id_test, '2019-12-03 11:00:00', '2020-02-13 10:00:00')  # first two weeks will be cut in get_samples
         train_df, _ = scaling(train_df)
         train_df = interpolate(train_df)
         train_data = get_samples(train_df)
@@ -169,6 +181,7 @@ def run(start=0, end=10000, epochs=6, device=0):
         test_df, maximum_predict = scaling(test_df)
         test_df = interpolate(test_df)
         test_data = get_samples_for_submission(test_df)
+
 
         # train
         model = Model(len(train_data[0][0]), len(train_data[0][1]))
@@ -183,23 +196,32 @@ def run(start=0, end=10000, epochs=6, device=0):
 
         # test
         predictions = predict_data(model, test_data, device, maximum_predict)
+        predictions_baseline = [float(scaling_inv(test_data[167][0][2], maximum_predict)) for i in range(168)]
+        predictions_baseline_last_week = [float(scaling_inv(test_data[0][0][2], maximum_predict)) for i in range(168)]
+
         ground_trouth_last_week = [float(scaling_inv(y, maximum_predict)) for x, y in test_data]
 
-        r2 = r2_score(y_true=ground_trouth_last_week, y_pred=predictions)
+        r2_last_week = r2_score(y_true=ground_trouth_last_week, y_pred=predictions)
+        r2_baseline_last_week = r2_score(y_true=ground_trouth_last_week, y_pred=predictions_baseline_last_week)
 
-        if r2 < -1.0:
-            print("R2 recalc: " + str(r2))
-            predictions = [float(scaling_inv(test_data[167][0][2], maximum_predict)) for i in range(168)]
+        print("R2: ")
+        if r2_baseline_last_week > r2_last_week:
+            predictions = predictions_baseline
 
         r2 = r2_score(y_true=ground_trouth_last_week, y_pred=predictions)
+        print("R2 last week: " + str(r2_last_week) + " R2 baseline last week: " + str(r2_baseline_last_week) + " --> decided: " + str(r2))
+
         r2_total += r2
+        r2_baseline_total += r2_baseline_last_week
 
         result.extend(predictions)
         submission_results = submission_results.append([result], ignore_index=True)
-        print(data_id_test + " R2 (if its the same like last week): " + str(r2))
+        print(data_id_test + " R2 (from last week): " + str(r2))
+        print("R2 Baseline AVG: " + str(r2_baseline_total / (end - start)))
+
 
     submission_results.to_csv("submission_thorsten_from{}_to{}.csv".format(start, end), header=False, index=False)
-    print("R2 AVG: " + str(r2_total/(end-start)))
+    print("R2 AVG: " + str(r2_total / (end-start)))
 
 
 def main():
