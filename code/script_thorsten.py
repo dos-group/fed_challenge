ff2190a941b3564fa26d78194780ea0161c6884f
twittkopp <62097606+twittkopp@users.noreply.github.com>
2020-06-30 15:21:34 +0200

    code for paper

--- code/script_thorsten.py
+++ code/script_thorsten.py
@@ -1,9 +1,10 @@
+import argparse
+
 import pandas as pd
 import sqlite3 as lite
 import torch
 import torch.nn as nn
 import time
-import argparse
 from sklearn.metrics import r2_score
 
 
@@ -32,13 +33,23 @@ def interpolate(test_df):
     return test_df
 
 
-def scaling(df):
+def inject_baseline(df, baseline):
+    df['baseline'] = baseline
+    return df
+
+
+def scaling(df, upper=100):
     maximum = df['Mean'].max()
     maximum_close = df['Close'].max()
+    maximum_baseline = df['baseline'].max()
     if maximum_close > maximum:
         maximum = maximum_close
-    df['Mean'] = df['Mean'].apply(lambda x: (x/maximum)*100)
-    df['Close'] = df['Close'].apply(lambda x: (x/maximum)*100)
+        if maximum_baseline > maximum:
+            maximum = maximum_baseline
+    if maximum > 0:
+        df['Mean'] = df['Mean'].apply(lambda x: (x/maximum)*upper)
+        df['Close'] = df['Close'].apply(lambda x: (x/maximum)*upper)
+        df['baseline'] = df['baseline'].apply(lambda x: (x/maximum)*upper)
     return df, maximum
 
 
@@ -60,7 +71,7 @@ def get_samples(df):
     last_value_mean = 0
     last_value_close = 0
     for i in range(len(df['Mean'])-one_week):
-        if i%one_week == 0:
+        if i % one_week == 0:
             last_value_mean = df['Mean'][i+one_week-1]
             last_value_close = df['Close'][i+one_week-1]
         last_values_mean.append(last_value_mean)
@@ -117,7 +128,6 @@ def train_models(model, data_id_test, train_data, epochs, device):
     print('######### Training {} #########'.format(prediction_series))
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
-    #loss_function = torch.nn.L1Loss()
     loss_function = torch.nn.MSELoss()
     model.cuda(device)
     losses = []
@@ -125,7 +135,6 @@ def train_models(model, data_id_test, train_data, epochs, device):
     for e in range(epochs):
         total_loss = 0.0
         for x, y in train_data:
-            #print("training_x: " + str(x) + " training_y: " + str(y))
             x_tensor = torch.tensor(x, dtype=torch.float).cuda(device)
             optimizer.zero_grad()
             out = model(x_tensor.cuda(device))
@@ -150,77 +159,77 @@ def predict_data(model, test_data, device, m):
             output = model(torch.tensor(x, dtype=torch.float).cuda(device))
             output = float(scaling_inv(output, m))
             predictions.append(output)
-            #print("predict_input: " + str(x) + " prediction: " + str(output))
     return predictions
 
 
 def run(start=0, end=10000, epochs=6, device=0):
 
-    solution = pd.read_csv('../data/solution_template.csv', header=None)
+    solution = pd.read_csv('../data/exemplary_solution.csv', header=None)
     db_connection = lite.connect('../data/series.db')
 
-    ids_df = solution[solution.columns[0:2]]
-    hosts = list(ids_df[0])
-    metrics = list(ids_df[1])
-    ids = [hosts[i] + "#" + metrics[i] for i in range(len(hosts))]
-    ids = ids[start:end]
+    ids_df = solution[solution.columns[0:3]]
+    ids_df = ids_df[start:end]
 
     r2_total = 0
-    r2_baseline_total = 0
 
     submission_results = pd.DataFrame()
-    for data_id_test in ids:
-        # Series and host
-        result = data_id_test.split("#")
+
+    for row in ids_df.itertuples():
+        # Series and host and baseline
+        v_host = row[1]
+        v_series = row[2]
+        v_baseline = row[3]
+
+        result = [v_host, v_series]
+        data_id_test = v_host + "#" + v_series
 
         train_df = get_dataframe(db_connection, data_id_test, '2019-12-03 11:00:00', '2020-02-20 10:00:00')  # first two weeks will be cut in get_samples
+        train_df = inject_baseline(train_df, v_baseline)
         train_df, _ = scaling(train_df)
         train_df = interpolate(train_df)
         train_data = get_samples(train_df)
 
-        test_df = get_dataframe(db_connection, data_id_test, '2020-02-06 11:00:00', '2020-02-20 10:00:00')
+        test_df = get_dataframe(db_connection, data_id_test, '2020-02-06 11:00:00', '2020-02-20 10:00:00')  # first two weeks will be cut in get_samples
+        test_df = inject_baseline(test_df, v_baseline)
         test_df, maximum_predict = scaling(test_df)
         test_df = interpolate(test_df)
         test_data = get_samples_for_submission(test_df)
-
 
         # train
         model = Model(len(train_data[0][0]), len(train_data[0][1]))
         losses = train_models(model, data_id_test, train_data, epochs=epochs, device=device)
 
-        max_iter = 10
-        while len(set(losses)) < 3 and max_iter > 0:
-            print("--------------------------> Nothing learned !!! ------------> try again xD")
-            model = Model(len(train_data[0][0]), len(train_data[0][1]))
-            losses = train_models(model, data_id_test, train_data, epochs=epochs, device=device)
-            max_iter -= 1
 
         # test
         predictions = predict_data(model, test_data, device, maximum_predict)
-        predictions_baseline = [float(scaling_inv(test_data[167][0][2], maximum_predict)) for i in range(168)]
-        predictions_baseline_last_week = [float(scaling_inv(test_data[0][0][2], maximum_predict)) for i in range(168)]
+        predictions_baseline = [v_baseline for i in range(168)]
 
         ground_trouth_last_week = [float(scaling_inv(y, maximum_predict)) for x, y in test_data]
 
+        predictions = [round(x, 4) for x in predictions]
+        ground_trouth_last_week = [round(x, 4) for x in ground_trouth_last_week]
+        print(predictions_baseline)
+        print(predictions)
+        print(ground_trouth_last_week)
+
         r2_last_week = r2_score(y_true=ground_trouth_last_week, y_pred=predictions)
-        r2_baseline_last_week = r2_score(y_true=ground_trouth_last_week, y_pred=predictions_baseline_last_week)
+        r2_baseline_last_week = r2_score(y_true=ground_trouth_last_week, y_pred=predictions_baseline)
+
+        print("R2 if it is similar to last week: " + str(r2_last_week) + " Real R2: " + " baseline last week " + str(r2_baseline_last_week))
 
         if r2_baseline_last_week > r2_last_week:
             predictions = predictions_baseline
 
         r2 = r2_score(y_true=ground_trouth_last_week, y_pred=predictions)
-        print("R2 last week: " + str(r2_last_week) + " R2 baseline last week: " + str(r2_baseline_last_week) + " --> decided: " + str(r2))
-
+        print("R2 taken: " + str(r2))
         r2_total += r2
-        r2_baseline_total += r2_baseline_last_week
 
         result.extend(predictions)
         submission_results = submission_results.append([result], ignore_index=True)
-        print(data_id_test + " R2 (from last week): " + str(r2))
+        print(data_id_test + " R2: " + str(r2))
 
     submission_results.to_csv("submission_thorsten_from{}_to{}.csv".format(start, end), header=False, index=False)
     print("R2 AVG: " + str(r2_total / (end-start)))
-    print("R2 Baseline AVG: " + str(r2_baseline_total / (end - start)))
 
 
 def main():
